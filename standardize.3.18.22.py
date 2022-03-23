f4c83080006b37eeedc64aa259a1d7913132bfcf
AB <am.barker8@gmail.com>
2022-03-23 10:11:11 -0500

    update standardize protocol

--- standardize.3.18.22.py
+++ standardize.3.18.22.py
@@ -2,8 +2,9 @@ import csv
 
 metadata = {
     'protocolName': 'Standardize DNA Concentrations',
-    'author': 'AMB, last updated 3/21/22',
-    'description': 'Add designated amounts of DNA and water to standardize samples to a target concentration',
+    'author': 'AMB, last updated 3/23/22',
+    'description': 'Adds designated amounts of DNA and water to wells in order to '
+                   'standardize samples to a target concentration',
     'apiLevel': '2.11'
 }
 
@@ -26,12 +27,12 @@ def run(protocol):
     # sample standardization info
     # paste data from csv here (in between '''  ''')
     input_data = '''
-        source_slot,source_well,dest_slot,dest_well,vol_dna,vol_water
-        1,A1,1,A1,40,60
-        1,C1,1,B1,66.67,33.33
-        2,D1,2,C1,20,80
-        3,E3,2,D1,10
-        '''
+    source_slot,source_well,dest_slot,dest_well,vol_dna,vol_water
+    1,A1,1,A1,40,60
+    1,C1,1,B1,66.67,33.33
+    2,D1,2,C1,20,80
+    3,E3,2,D1,10,90
+    '''
 
 
 ########## DO NOT EDIT BELOW THIS LINE ##########
@@ -52,7 +53,7 @@ def run(protocol):
         raise Exception("Invalid number of destination plates. Must be 1-2")
 
     # Define hardware, pipettes/tips, plates
-    # specify tips and slots
+    # load tips
     tips300 = [
      protocol.load_labware(
       'opentrons_96_tiprack_300ul', str(slot)) for slot in [
@@ -65,221 +66,64 @@ def run(protocol):
     p300 = protocol.load_instrument("p300_single_gen2", mount=pipette_mount_300, tip_racks=tips300)
     p20 = protocol.load_instrument("p20_single_gen2", mount=pipette_mount_20, tip_racks=tips20)
 
-    # specify reagent labware, reagents in each well, and slot on deck
+    # load reagent labware and specify reagents in each well/columns
     water_reservoir = protocol.load_labware(
         'nest_12_reservoir_15ml', '4')
-    [water] = [reagent_reservoir.wells_by_name()[well] for well in ['A1']]
+    water = [water_reservoir.wells_by_name()[well] for well in ['A1']]
+
+    # load plates
     # add first source DNA plate
-    source_P1 = [protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', '1')]
+    protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', '1')
     #if more than one plate, add 2nd
     if num_source_plates > 1:
-        source_P2 = [protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', '2')]
+        protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', '2')
     # if more than 2 plates, add 3rd
     if num_source_plates > 2:
-        source_P3 = [protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', '3')]
+        protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', '3')
     # add first DNA destination plate
-    dest_P1 = [protocol.load_labware('biorad_96_wellplate_200ul_pcr', '5')]
+    protocol.load_labware('biorad_96_wellplate_200ul_pcr', '5')
     # if more than one plate, add second
     if num_destination_plates > 1:
-        dest_P2 = [protocol.load_labware('biorad_96_wellplate_200ul_pcr', '6')]
+        protocol.load_labware('biorad_96_wellplate_200ul_pcr', '6')
 
     # parse input data
-    csv_data = input_data.splitlines()[1:] #discard first blank line from data
-    csv_reader = csv.DictReader(csv_data)
-    for csv_row in csv_reader:
-        source_slot = csv_row['source_slot']
-        source_well = csv_row['source_well']
-        dest_slot = csv_row['dest_slot']
-        dest_well = csv_row['dest_well']
-        vol_dna = float(csv_row['vol_dna'])
-        vol_water = float(csv_row['vol_water'])
+    csv_data = [[val.strip() for val in line.split(',')]
+                for line in input_data.splitlines()
+                if line.split(',')[0].strip()][1:]
 
-    # transfer water
-    for line in csv_row:
-        if vol_water > 0 and vol_water <= 20:
-            p20.transfer(vol_dna, water_reservoir.water, )
+    # add water to all destination wells
+    # first have both pipettes pick up tips
+    p20.pick_up_tip()
+    p300.pick_up_tip()
+    # loops through wells and add water
+    for row in csv_data:
+        dest_well = protocol.loaded_labwares[int(row[2])].wells_by_name()[row[3]]
+        vol_water = float(row[5])
+        # use p20 if appropriate volume. Dispense at top of well so nothing touches and you can ues the same tip
+        if 0 < vol_water <= 20:
+            p20.transfer(vol_water, water, dest_well.top(1), blow_out=True,
+                        blowout_location='destination well', new_tip='never')
+        # use p300 if appropriate volume. Dispense at top of well so nothing touches and you can ues the same tip
+        if vol_water > 20:
+            p300.transfer(vol_water, water, dest_well.top(1), blow_out=True,
+                        blowout_location='destination well', new_tip='never')
+    # drop tips after all water has been added
+    p20.drop_tip()
+    p300.drop_tip()
 
-
-
-
-    # Add ampure/PEG
-    for index, column in enumerate(mag_plate.columns()[:num_cols]):
-        ccst = capture_current_starting_tip()
-        name_the_tips(ccst[0], ['sup_tips'], [
-            named_tips['current_tip'].well_name])
-        p300.pick_up_tip()
-        # if adding beads, mix 2 times before pipetting
-        p300.flow_rate.aspirate = 100
-        p300.flow_rate.dispense = 100
-        if beads_peg == 'beads':
-            p300.mix(2, 150, beads.bottom(1))
-        p300.flow_rate.aspirate = 50
-        p300.flow_rate.dispense = 50
-        # add specified volume of beads/PEG to samples
-        p300.transfer(
-         bead_volume, beads.bottom(1), column[0].bottom(3), new_tip='never')
-        # pipette up and down 5 times
-        p300.mix(5, bead_volume, column[0].bottom(2))
-        p300.move_to(column[0].top())
-        p300.blow_out()
-        # touch pipette tip to side of wells to knock off any remaining liquid
-        p300.touch_tip()
-        p300.return_tip()
-
-    # Incubate beads and DNA at RT
-    msg = "Incubating the beads for "+str(incubation_time)+" minutes. \
-    Protocol will resume automatically."
-    protocol.delay(minutes=incubation_time, msg=msg)
-
-    # Engage magdeck
-    mag_deck.engage()
-    protocol.comment("Delaying for "+str(settling_time)+" minutes for \
-beads to settle. Protocol will resume automatically.")
-    protocol.delay(minutes=settling_time)
-
-    # Aspirate supernatant
-    for index, column in enumerate(mag_plate.columns()[:num_cols]):
-        p300.pick_up_tip(named_tips['sup_tips'][index])
-        p300.move_to(column[0].top())
-        # aspirate really slowly to limit amount of beads
-        p300.flow_rate.aspirate = 5
-        p300.flow_rate.dispense = 100
-        # limits up/down movement speed of pipette
-        protocol.max_speeds['Z'] = 10
-        p300.aspirate(total_vol, column[0].bottom(1))
-        p300.move_to(column[0].top())
-        protocol.max_speeds['Z'] = None
-        protocol.delay(seconds=1)
-        # dispense in waste reservoir
-        p300.dispense(total_vol, waste_reservoir.wells()[0].top())
-        p300.blow_out(waste_reservoir.wells()[0].top())
-        p300.return_tip()
-
-    # EtOH wash 1
-    # loop through groups
-    for g in range(0, num_groups):
-        p300.flow_rate.aspirate = 100
-        p300.flow_rate.dispense = 150
-        p300.pick_up_tip()
-        # add ethanol to each column in current group
-        for index, column in enumerate(mag_plate.columns()[grps[g][0]:grps[g][1]], grps[g][0]):
-            p300.transfer(
-                etoh_volume, etoh_1.bottom(1), column[0].top(-1), new_tip='never')
-            p300.move_to(column[0].top())
-            p300.blow_out()
-            protocol.delay(seconds=1)
-        p300.drop_tip()
-
-        # Incubate etoh
-        # if 3 columns in group, go straight to aspirate
-        if grps[g][1]-grps[g][0] == 3:
-            pass
-        # if 1-2 columns, delay for appropriate amount of time
-        elif grps[g][1] - grps[g][0] == 2:
-            etoh_time = 5
-            protocol.comment("Let ethanol sit for " + str(etoh_time) + " seconds. Protocol will resume automatically.")
-            protocol.delay(seconds=etoh_time)
-        elif grps[g][1] - grps[g][0] == 1:
-            etoh_time = 8
-            protocol.comment("Let ethanol sit for " + str(etoh_time) + " seconds. Protocol will resume automatically.")
-            protocol.delay(seconds=etoh_time)
-
-        # Aspirate etoh using parked tips
-        for index, column in enumerate(mag_plate.columns()[grps[g][0]:grps[g][1]], grps[g][0]):
-            p300.flow_rate.aspirate = 40
-            p300.flow_rate.dispense = 150
-            p300.pick_up_tip(named_tips['sup_tips'][index])
-            p300.move_to(column[0].top())
-            protocol.max_speeds['Z'] = 10
-            p300.aspirate(etoh_volume, column[0].bottom(1))
-            p300.move_to(column[0].top())
-            protocol.max_speeds['Z'] = None
-            protocol.delay(seconds=1)
-            p300.air_gap(10)
-            p300.dispense(etoh_volume + 10, waste_reservoir.wells()[0].top())
-            p300.blow_out(waste_reservoir.wells()[0].top())
-            p300.return_tip()
-
-    # EtOH wash 2
-    for g in range(0, num_groups):
-        p300.flow_rate.aspirate = 100
-        p300.flow_rate.dispense = 150
-        p300.pick_up_tip()
-        for index, column in enumerate(mag_plate.columns()[grps[g][0]:grps[g][1]], grps[g][0]):
-            p300.transfer(
-                etoh_volume, etoh_2.bottom(1), column[0].top(-1), new_tip='never')
-            p300.move_to(column[0].top())
-            p300.blow_out()
-            protocol.delay(seconds=1)
-        p300.drop_tip()
-
-        # Incubate etoh
-        # if 3 columns in group, go straight to aspirate
-        if grps[g][1]-grps[g][0] == 3:
-            pass
-        # if 1-2 columns, delay for appropriate amount of time
-        elif grps[g][1] - grps[g][0] == 2:
-            etoh_time = 5
-            protocol.comment("Let ethanol sit for " + str(etoh_time) + " seconds. Protocol will resume automatically.")
-            protocol.delay(seconds=etoh_time)
-        elif grps[g][1] - grps[g][0] == 1:
-            etoh_time = 8
-            protocol.comment("Let ethanol sit for " + str(etoh_time) + " seconds. Protocol will resume automatically.")
-            protocol.delay(seconds=etoh_time)
-
-        # Aspirate etoh
-        for index, column in enumerate(mag_plate.columns()[grps[g][0]:grps[g][1]], grps[g][0]):
-            p300.flow_rate.aspirate = 40
-            p300.flow_rate.dispense = 150
-            p300.pick_up_tip(named_tips['sup_tips'][index])
-            p300.move_to(column[0].top())
-            protocol.max_speeds['Z'] = 10
-            p300.aspirate(etoh_volume, column[0].bottom(1))
-            p300.move_to(column[0].top())
-            protocol.max_speeds['Z'] = None
-            protocol.delay(seconds=1)
-            p300.air_gap(10)
-            p300.dispense(etoh_volume+10, waste_reservoir.wells()[0].top())
-            p300.blow_out(waste_reservoir.wells()[0].top())
-            p300.drop_tip()
-
-    # Dry at RT
-    msg = "Drying the beads for " + str(drying_time) + " minutes. Protocol \
-        will resume automatically."
-    protocol.delay(minutes=drying_time, msg=msg)
-
-    # Disengage MagDeck
-    mag_deck.disengage()
-
-    # Elute DNA
-    p300.flow_rate.aspirate = 50
-    p300.flow_rate.dispense = 50
-    for index, column in enumerate(mag_plate.columns()[:num_cols]):
-        p300.pick_up_tip()
-        # add water/elution buffer to samples
-        p300.transfer(
-            elution_buffer_volume, water.bottom(1), column[0].bottom(3), new_tip='never'
-        )
-        # pipette up and down 5 times
-        p300.mix(5, elution_buffer_volume, column[0].bottom(2))
-        p300.move_to(column[0].top())
-        p300.blow_out()
-        # touch pipette tip to sides of well to knock off remaining liquid
-        p300.touch_tip()
-        p300.drop_tip()
-
-    # Incubate at RT
-    protocol.comment("Incubating the beads for "+str(settling_time)+" minutes. \
-    Protocol will resume automatically.")
-    protocol.delay(minutes=incubation_time)
-
-    # engage magnet to clear beads after final elution
-    if final_clear is True:
-        mag_deck.engage()
-        protocol.comment("Delaying for "+str(settling_time)+" minutes for \
-            beads to settle.")
-        protocol.delay(minutes=settling_time)
+    # add dna to each well
+    for row in csv_data:
+        source_well = protocol.loaded_labwares[int(row[0])].wells_by_name()[row[1]]
+        dest_well = protocol.loaded_labwares[int(row[2])].wells_by_name()[row[3]]
+        vol_dna = float(row[4])
+        # use p20 if appropriate volume
+        if 0 < vol_dna <= 20:
+            p20.transfer(vol_dna, source_well, dest_well, blow_out=True,
+                        blowout_location='destination well', new_tip='always')
+        # use p300 if appropriate volume
+        if vol_dna > 20:
+            p300.transfer(vol_dna, source_well, dest_well, blow_out=True,
+                        blowout_location='destination well', new_tip='always')
 
     # turn off lights
     protocol.set_rail_lights(False)
-
