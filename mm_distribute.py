c0a3f2b2af405bf1dfc4e81d15768759a1974a98
AB <am.barker8@gmail.com>
2022-03-28 14:09:54 -0500

    update master mix distribute protocol

--- mm_distribute.py
+++ mm_distribute.py
@@ -23,16 +23,16 @@ def run(protocol):
     # number of samples in each plate (list of numbers, surrounded by brackets and separated by commas)
     # ex: [96, 12, 72]
     # (will go by rows, i.e. 12 would mean samples A1-A12)
-    num_samples_each_plate = [92, 72]
+    num_samples_each_plate = [3, 4]
 
     # list wells that should be skipped (well name in single quotes and in brackets. Multiple wells separated by commas
-    # ex: ['C2, 'D11, 'E1']
+    # ex: ['C2', 'D11', 'E1']
     # these samples should be included in the sample count above
     # ex: you want the protocol to distribute all the say to A12, but skip A10
     # you sample count would be 12 and then you would indicate to skip A10: P1_skip = [`A10']
     # must indicate what to skip for each plate, if nothing than don't put anything between brackets
-    P1_skip = []
-    P2_skip = []
+    P1_skip = ['A1', 'A2']
+    P2_skip = ['A4']
     P3_skip = []
     P4_skip = []
     P5_skip = []
@@ -41,7 +41,7 @@ def run(protocol):
     plate_type = 'biorad_200ul'
 
     # volume (ul) of master mix to add to each well
-    mm_vol = 12
+    mm_vol = 11.1
 
     # mix after pipetting master mix into sample ('yes' or 'no', all lowercase and in single quotes)
     mix_sample = 'no'
@@ -132,7 +132,7 @@ def run(protocol):
             plate = plate_names[num_plate]
             sample_name = well_names[sample]
             # move on to next iteration of loop if sample name is in the skip list for this plate
-            if sample_name in wells_skip[num_plate] is True:
+            if sample_name in wells_skip[num_plate]:
                 continue
             if mix_sample == 'yes':
                 p20.transfer(mm_vol, mm_tube, plate.wells_by_name()[sample_name], mix_after=(3, mm_vol),
@@ -141,7 +141,7 @@ def run(protocol):
                 p20.transfer(mm_vol, mm_tube, plate.wells_by_name()[sample_name],
                              blow_out=True, blowout_location='destination well', touch_tip=True, new_tip='always')
             else:
-                raise Exception ('Must indicate if sample should be mixed after adding master mix')
+                raise Exception('Must indicate if sample should be mixed after adding master mix')
 
     # turn off lights when protocol compelte
     protocol.set_rail_lights(False)
